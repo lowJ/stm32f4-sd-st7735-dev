24cb65d89b3765fcbf5d9f258c83a219a8cc8092
Jon <jwlow@uci.edu>
2025-06-16 22:45:33 -0700

    play in loop

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -163,6 +163,18 @@ int main(void)
       if(br != 2*128*160)
       {
         SEGGER_RTT_printf(0, "EOF? Read %dKB", bytes_read / 1000 );
+        f_close( &SDFile ); /* close file */
+
+        FRESULT res = f_open(&SDFile,filename, FA_OPEN_EXISTING | FA_READ );
+        if(res == FR_OK)
+        {
+          SEGGER_RTT_printf(0, "File opened");
+        }
+        else
+        {
+          SEGGER_RTT_printf(0, "could not open file %d", res);
+          Error_Handler();
+        }
         //Error_Handler();
       }
       bytes_read += br;
@@ -173,79 +185,8 @@ int main(void)
       Error_Handler();
     }
 
-    // ST7735_FillScreen(ST7735_BLACK);
-
-    // for(int x = 0; x < ST7735_WIDTH; x++) {
-    //     ST7735_DrawPixel(x, 0, ST7735_RED);
-    //     ST7735_DrawPixel(x, ST7735_HEIGHT-1, ST7735_RED);
-    // }
-
-    // for(int y = 0; y < ST7735_HEIGHT; y++) {
-    //     ST7735_DrawPixel(0, y, ST7735_RED);
-    //     ST7735_DrawPixel(ST7735_WIDTH-1, y, ST7735_RED);
-    // }
-
-    // //HAL_Delay(3000);
-
-    // // Check fonts
-    // ST7735_FillScreen(ST7735_BLACK);
-    // ST7735_WriteString(0, 0, "Font_7x10, red on black, lorem ipsum dolor sit amet", Font_7x10, ST7735_RED, ST7735_BLACK);
-    // ST7735_WriteString(0, 3*10, "Font_11x18, green, lorem ipsum", Font_11x18, ST7735_GREEN, ST7735_BLACK);
-    // ST7735_WriteString(0, 3*10+3*18, "Font_16x26", Font_16x26, ST7735_BLUE, ST7735_BLACK);
-    // //HAL_Delay(2000);
-
-    // // Check colors
-    // ST7735_FillScreen(ST7735_BLACK);
-    // ST7735_WriteString(0, 0, "BLACK", Font_11x18, ST7735_WHITE, ST7735_BLACK);
-    // //HAL_Delay(500);
-
-    // ST7735_FillScreen(ST7735_BLUE);
-    // ST7735_WriteString(0, 0, "BLUE", Font_11x18, ST7735_BLACK, ST7735_BLUE);
-    // //HAL_Delay(500);
-
-    // ST7735_FillScreen(ST7735_RED);
-    // ST7735_WriteString(0, 0, "RED", Font_11x18, ST7735_BLACK, ST7735_RED);
-    // //HAL_Delay(500);
-
-    // ST7735_FillScreen(ST7735_GREEN);
-    // ST7735_WriteString(0, 0, "GREEN", Font_11x18, ST7735_BLACK, ST7735_GREEN);
-    // //HAL_Delay(500);
-
-    // ST7735_FillScreen(ST7735_CYAN);
-    // ST7735_WriteString(0, 0, "CYAN", Font_11x18, ST7735_BLACK, ST7735_CYAN);
-    // //HAL_Delay(500);
-
-    // ST7735_FillScreen(ST7735_MAGENTA);
-    // ST7735_WriteString(0, 0, "MAGENTA", Font_11x18, ST7735_BLACK, ST7735_MAGENTA);
-    // //HAL_Delay(500);
-
-    // ST7735_FillScreen(ST7735_YELLOW);
-    // ST7735_WriteString(0, 0, "YELLOW", Font_11x18, ST7735_BLACK, ST7735_YELLOW);
-    // //HAL_Delay(500);
-
-    // ST7735_FillScreen(ST7735_WHITE);
-    // ST7735_WriteString(0, 0, "WHITE", Font_11x18, ST7735_BLACK, ST7735_WHITE);
-    // //HAL_Delay(500);
-
-    // img_buf[pixeli]  = color;
-    // pixeli = pixeli + 1;
-    // if(pixeli >= 168*128)
-    // {
-    //   color = color - 100;
-    //   pixeli = 0;
-    // }
-
-    // for(int i = 0; i < 128*160; i++ )
-    // {
-    //   img_buf[i] = color;
-    //   color--;
-    // }
-
-
-
   ST7735_DrawImage(0, 0, 160, 128, (uint16_t*)buf);
   total_frames++;
-
 
     /* 1s task */
     if( ( time_ms - last_toggle_ms) >= blink_duration_ms )
